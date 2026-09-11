9435dcf388f535841c581de254ffeccc50d0d4a0
Vince1993 <v,huegen@icloud.com>
2026-09-11 15:06:31 +0200

    sprint review1

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
 document.getElementById('menu_button').addEventListener('click', () => {
   document.querySelector('.nav-wrapper').classList.toggle('open');
 });
+
+document.getElementById("img_fuwate_reload").addEventListener("click", function () {
+  window.location.href = "index.html";
+});
+
+document.getElementById("img_sg").addEventListener("click", function () {
+  window.open("https://www.sg74.de/", "_blank");
+});
+
+document.getElementById('card_button').addEventListener('click', () => {
+  window.open("fuwate-card.html", "_blank");
+});
